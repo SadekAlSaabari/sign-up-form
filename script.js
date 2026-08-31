2bcd84365b57be191ae0ef006b6be4eaf74a8c51
Sadek <186719556+SadekAlSaabari@users.noreply.github.com>
2026-08-31 23:56:33 +0100

    added signup functionality where the user's name, email and password will be saved

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 // catch signup form
-
 const signupForm = document.getElementById("signup-form");
 
 signupForm.addEventListener('submit', function(event) {
     event.preventDefault();
-    console.log("Form submitted!");
+    const name = document.getElementById('fname').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value; 
+
+    const data = JSON.parse(sessionStorage.getItem("users")) || [];
+
+    const userCheck = data.find(user => user.email === email);
+
+    if (userCheck) {
+        alert("An account with this email already exists!");
+        return;
+    }
+
+    data.push({name, email, password});
+    sessionStorage.setItem("users", JSON.stringify(data));
+    alert("Form submitted! Welcome aboard! \n You can try logging in if you want.");
 });
